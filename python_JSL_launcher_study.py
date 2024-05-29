e6cc3ca5b984262d7c5a95c18f9eb2d54b50b098
Bob Gotchall <bob.g@sigmasense.com>
2024-05-29 10:23:09 -0500

    adding initial JSL script

--- python_JSL_launcher_study.py
+++ python_JSL_launcher_study.py
@@ -1,3 +1,9 @@
 #just see if I can launch a simple JSL script from here.  This might be the best way to handle the top executive.
 
 
+import subprocess
+
+
+
+cmd = ["jmp.exe", "JSL_sample_script.jsl"]
+subprocess.run(cmd)             
